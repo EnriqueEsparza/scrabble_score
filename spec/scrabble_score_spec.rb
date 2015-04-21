8530e5abd2bc920afadb6dc62c3a72a29e348d24
Enrique & peter <student@epicodus.com>
2015-04-21 13:34:26 -0700

    third test rspec

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -10,4 +10,11 @@ it("returns a scrabble score for a word") do
   expect("table".scrabble_score()).to(eq(7))
 
 end
+
+it("returns a scrabble score for a word") do
+  expect("quibble".scrabble_score()).to(eq(20))
+
+end
+
+
 end
