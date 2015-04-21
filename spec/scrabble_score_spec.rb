c24b400a32b27fc881f4d3d45b76cbbdbc8362e4
Enrique & peter <student@epicodus.com>
2015-04-21 12:15:10 -0700

    second rspec test fail

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,6 +4,10 @@ require('scrabble_score')
 describe('String#scrabble_score') do
 it("returns a scrabble score for a letter") do
   expect("a".scrabble_score()).to(eq(1))
+end
+
+it("returns a scrabble score for a word") do
+  expect("table".scrabble_score()).to(eq(7))
 
 end
 end
