e7eaa5aacbf906f49a0a2183aa545aa8a17aea74
Enrique & peter <student@epicodus.com>
2015-04-21 13:32:34 -0700

    second test pass

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,10 +1,20 @@
 class String
   define_method(:scrabble_score) do
+    words = self.split("")
+ score = 0
 
       letter_score = Hash.new()
       letter_score.store("a", 1)
 
-      letter_score.fetch(self)
+      letter_score.store("t", 1)
+      letter_score.store("e", 1)
+      letter_score.store("b", 3)
+      letter_score.store("l", 1)
 
+
+      words.each do |letter|
+        score = score + letter_score.fetch(letter)
+      end
+        score
   end
-end  
+end
